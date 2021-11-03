0e6a9c37cdf68edb3cd179b76ab6cf039c42991d
acholonu <ucacholonu@hotmail.com>
2021-11-03 17:41:05 -0500

    Slack configuration

--- .circleci/ansible/roles/configure-prometheus-alertmanager/files/slack_config.py
+++ .circleci/ansible/roles/configure-prometheus-alertmanager/files/slack_config.py
@@ -4,7 +4,7 @@ from yaml import load, dump
 def main():
     # Get environment variable that store webhook url
     content = {}
-    content['slack_api_url'] = os.environ['SLACK_WEBHOOK'] #this will error out if key doesn't exist
+    content['slack_api_url'] = os.environ['slack_api_url'] #this will error out if key doesn't exist
     # WEBHOOK = os.getenv['SLACK_WEBHOOK'] # returns None if key doesn't exist
     with open('slack_configuration.yml', 'w') as file:
         documents = dump(content, file)
